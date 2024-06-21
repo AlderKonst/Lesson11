76f0a09807756e9573810d1fa3b5a4ff8962c604
AlderKonst <koalder@yandex.ru>
2024-06-21 20:33:11 +0300

    Добавил простейший декоратор @decorator_print() и 2 обработки исключений

--- .venv/borndayforewer.py
+++ .venv/borndayforewer.py
@@ -1,16 +1,35 @@
+# Вот такой вот простейший декоратор
+def decorator_print():
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            result = f(*args, **kwargs)
+            print("Викторина успешно завершена!")
+            return result
+        return wrapper
+    return decorator
+
+@decorator_print()
 def victorina():
     def year_day():
-        year = int(input('Ввведите год рождения А.С.Пушкина:'))
-        while year != '1799':
-            print("Не верно")
-            year = int(input('Ввведите год рождения А.С.Пушкина:'))
+        while True:
+            try:
+                year = int(input('Ввведите год рождения А.С.Пушкина:'))
+                if year == 1799:
+                    print("Верно")
+                    break
+                print("Не верно")
+            except ValueError:
+                print("Вы должны ввести число")
         day()
-        assert type(year) is int, 'Нужно вводить числа целые'
+
     def day():
-        day = int(input('Ввведите день рождения Пушкин?'))
-        while day != '6':
-            print("Не верно")
-            day = int(input('В какой день июня родился Пушкин?'))
-            assert type(day) is int, 'Нужно вводить числа целые'
-        print('Верно')
+        while True:
+            try:
+                day = int(input('Вввените день рождения Пушкин?'))
+                if day == 6:
+                    print('Верно')
+                    break
+                print("Не верно")
+            except ValueError:
+                print("Вы должны ввести число")
     year_day()
